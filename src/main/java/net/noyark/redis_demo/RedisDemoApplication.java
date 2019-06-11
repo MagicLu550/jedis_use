43e17d55aec11b477ab29bf85d9a0c7378de0789
Magiclu550 <843983728@qq.com>
2019-06-11 17:16:56 +0800

    Cluster

--- src/main/java/net/noyark/redis_demo/RedisDemoApplication.java
+++ src/main/java/net/noyark/redis_demo/RedisDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
 
@@ -14,6 +15,9 @@ public class RedisDemoApplication {
 
     @Autowired
     private ShardedJedisPool pool;
+
+    @Autowired
+    private JedisCluster cluster;
 
     public static void main(String[] args) {
         SpringApplication.run(RedisDemoApplication.class, args);
@@ -25,6 +29,12 @@ public class RedisDemoApplication {
         jedis.set("1","2");//
         pool.close();
         System.out.println("设置成功");
+    }
+    //测试代码端高可用
+    @RequestMapping
+    public String clusterSetGet(String key,String value){
+        cluster.set(key,value);
+        return cluster.get(key);
     }
 
 
